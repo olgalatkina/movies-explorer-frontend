0da9d775945326c2b6dcfce2ab236a1edf428ca3
Olga Latkina <olga.latkina@gmail.com>
2022-10-19 06:17:15 +0700

    feat: add Login markup and styles

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,50 @@
+import { Link } from 'react-router-dom';
 import './Login.css';
+import logo from '../../images/logo.svg';
 
 const Login = () => {
   return (
-    <section className='login'>Login</section>
+    <section className='login'>
+      <div className='login__wrapper'>
+        <Link to='/' className='login__logo-link'>
+          <img src={logo} alt='Логотип учебного проекта' className='logo' />
+        </Link>
+        <h1 className='login__title'>Рады видеть!</h1>
+      </div>
+
+      <form
+        className='form'
+        action=''
+        method=''
+      >
+        <label htmlFor='email' className='form__label'>E-mail
+          <input
+            className='form__input'
+            type='email'
+            name='email'
+            id='email'
+            required
+          />
+          <span className='form__error' id='email-error' />
+        </label>
+
+        <label htmlFor='email' className='form__label'>Пароль
+          <input
+            className='form__input'
+            type='password'
+            name='password'
+            id='password'
+            required
+          />
+          <span className='form__error' id='password-error' />
+        </label>
+        <button type='submit' className='form__submit-btn'>Войти</button>
+      </form>
+
+      <p className='login__question'>Ещё не зарегистрированы?&nbsp;
+        <Link to='/signup' className='login__signup-link'>Регистрация</Link>
+      </p>
+    </section>
   )
 };
 
