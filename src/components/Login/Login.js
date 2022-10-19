f8d8f4bd9c292e51a54e17f0fe90269a0cdcb189
Olga Latkina <olga.latkina@gmail.com>
2022-10-19 15:16:13 +0700

    refactor: add SignWithForm remove Form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,49 +1,41 @@
-import { Link } from 'react-router-dom';
-import './Login.css';
-import logo from '../../images/logo.svg';
-import Form from '../Form/Form';
+import SignWithForm from '../SignWithForm/SignWithForm';
 
 const Login = () => {
-  const buttonText = 'Войти';
-  const formName = 'login';
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+  };
 
   return (
-    <section className='login'>
-      <div className='login__wrapper'>
-        <Link to='/' className='login__logo-link'>
-          <img src={logo} alt='Логотип учебного проекта' className='logo' />
-        </Link>
-        <h1 className='login__title'>Рады видеть!</h1>
-      </div>
+    <SignWithForm
+      buttonText='Войти'
+      formName='login'
+      question='Ещё не зарегистрированы?'
+      linkText='Регистрация'
+      link='signup'
+      onSubmit={handleSubmit}
+    >
+      <label htmlFor='email' className='form__label'>E-mail
+        <input
+          className='form__input'
+          type='email'
+          name='email'
+          id='email'
+          required
+        />
+        <span className='form__error' id='email-error' />
+      </label>
 
-      <Form buttonText={buttonText} formName={formName}>
-        <label htmlFor='email' className='form__label'>E-mail
-          <input
-            className='form__input'
-            type='email'
-            name='email'
-            id='email'
-            required
-          />
-          <span className='form__error' id='email-error' />
-        </label>
-
-        <label htmlFor='email' className='form__label'>Пароль
-          <input
-            className='form__input'
-            type='password'
-            name='password'
-            id='password'
-            required
-          />
-          <span className='form__error' id='password-error' />
-        </label>
-      </Form>
-
-      <p className='login__question'>Ещё не зарегистрированы?&nbsp;
-        <Link to='/signup' className='login__signup-link'>Регистрация</Link>
-      </p>
-    </section>
+      <label htmlFor='email' className='form__label'>Пароль
+        <input
+          className='form__input'
+          type='password'
+          name='password'
+          id='password'
+          required
+        />
+        <span className='form__error' id='password-error' />
+      </label>
+    </SignWithForm>
   )
 };
 
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,60 +1,52 @@
-import { Link } from 'react-router-dom';
-import './Register.css';
-import logo from '../../images/logo.svg';
-import Form from '../Form/Form';
+import SignWithForm from "../SignWithForm/SignWithForm";
 
 const Register = () => {
-  const buttonText = 'Зарегистрироваться';
-  const formName = 'register';
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+  };
 
   return (
-    <section className='login'>
-      <div className='login__wrapper'>
-        <Link to='/' className='login__logo-link'>
-          <img src={logo} alt='Логотип учебного проекта' className='logo' />
-        </Link>
-        <h1 className='login__title'>Добро пожаловать!</h1>
-      </div>
+    <SignWithForm
+      buttonText='Зарегистрироваться'
+      formName='register'
+      question='Уже зарегистрированы?'
+      linkText='Войти'
+      link='signin'
+      onSubmit={handleSubmit}
+    >
+      <label htmlFor='name' className='form__label'>Имя
+        <input
+          className='form__input'
+          type='text'
+          name='name'
+          id='name'
+          required
+        />
+        <span className='form__error' id='email-error' />
+      </label>
 
-      <Form buttonText={buttonText} formName={formName}>
-        <label htmlFor='name' className='form__label'>Имя
-          <input
-            className='form__input'
-            type='text'
-            name='name'
-            id='name'
-            required
-          />
-          <span className='form__error' id='email-error' />
-        </label>
+      <label htmlFor='email' className='form__label'>E-mail
+        <input
+          className='form__input'
+          type='email'
+          name='email'
+          id='email'
+          required
+        />
+        <span className='form__error' id='email-error' />
+      </label>
 
-        <label htmlFor='email' className='form__label'>E-mail
-          <input
-            className='form__input'
-            type='email'
-            name='email'
-            id='email'
-            required
-          />
-          <span className='form__error' id='email-error' />
-        </label>
-
-        <label htmlFor='email' className='form__label'>Пароль
-          <input
-            className='form__input'
-            type='password'
-            name='password'
-            id='password'
-            required
-          />
-          <span className='form__error' id='password-error' />
-        </label>
-      </Form>
-
-      <p className='login__question'>Уже зарегистрированы?&nbsp;
-        <Link to='/signin' className='login__signup-link'>Войти</Link>
-      </p>
-    </section>
+      <label htmlFor='email' className='form__label'>Пароль
+        <input
+          className='form__input'
+          type='password'
+          name='password'
+          id='password'
+          required
+        />
+        <span className='form__error' id='password-error' />
+      </label>
+    </SignWithForm>
   )
 };
 
