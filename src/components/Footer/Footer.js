2c7b7be8b3fb825f36bf15999ae8d3c39c49b4ef
Olga Latkina <olga.latkina@gmail.com>
2022-10-20 05:02:43 +0700

    feat: add Footer markup and styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,16 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <footer className='footer'>Footer</footer>
+    <footer className='footer'>
+      <div className='footer__content'>
+        <p className='footer__text'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
+        <div className='footer__wrapper'>
+          <a href='https://practicum.yandex.ru/' className='footer__link' target='_blank'>Яндекс.Практикум</a>
+          <a href='https://github.com/olgalatkina' className='footer__link' target='_blank'>Github</a>
+          <p className='footer__copyright'>olgalatkina&copy;2020</p>
+        </div>
+      </div>
+    </footer>
   )
 };
 
