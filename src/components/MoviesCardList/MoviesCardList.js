1c8f115fea6f81ce8b6f7e4b7e38fcb774358886
Olga Latkina <olga.latkina@gmail.com>
2022-10-20 16:30:13 +0700

    feat: add MoviesCardList markup and styles

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,8 +1,29 @@
 import './MoviesCardList.css';
+import MoviesCard from '../MoviesCard/MoviesCard';
+import movies from '../../utils/mock';
+console.log(movies[0]);
 
-const MoviesCardList = () => {
+const MoviesCardList = ({ movies }) => {
   return (
-    <ul className='card-list' />
+    <section className='cards'>
+      <div className='cards__content'>
+        <ul className='cards__list'>
+          <MoviesCard />
+          {/*{movies.map((movie) => (*/}
+          {/*  <MoviesCard*/}
+          {/*    key={`${movie.id}-${movie.name}`}*/}
+          {/*    movie={movie}*/}
+          {/*  />*/}
+          {/*))}*/}
+        </ul>
+        <button
+          className='cards__more-btn button'
+          type='button'
+        >
+          Ещё
+        </button>
+      </div>
+    </section>
   )
 };
 
