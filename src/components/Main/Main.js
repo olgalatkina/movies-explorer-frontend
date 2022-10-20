2040dd60fe28300118e32c1d83d56032c61223ff
Olga Latkina <olga.latkina@gmail.com>
2022-10-21 02:17:14 +0700

    feat: add Promo markup and styles

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,20 @@
 import './Main.css';
+import Promo from '../Promo/Promo';
+import NavTab from '../NavTab/NavTab';
+import AboutProject from '../AboutProject/AboutProject';
+import Techs from '../Techs/Techs';
+import AboutMe from '../AboutMe/AboutMe';
+import Portfolio from '../Portfolio/Portfolio';
 
 const Main = () => {
   return (
     <main>
-      Main
+      <Promo />
+      <NavTab />
+      <AboutProject />
+      <Techs />
+      <AboutMe />
+      <Portfolio />
     </main>
   )
 };
