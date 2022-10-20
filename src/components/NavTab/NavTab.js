3ee350320f927f4ff6410ddff32d6964e7a66785
Olga Latkina <olga.latkina@gmail.com>
2022-10-21 02:48:05 +0700

    feat: add NavTab markup and styles

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -2,9 +2,21 @@ import './NavTab.css';
 
 const NavTab = () => {
   return (
-    <div className='nav-tab'>
-
-    </div>
+    <nav className='nav-tab'>
+      <div className='nav-tab__content'>
+        <ul className='nav-tab__list'>
+          <li className='nav-tab-item'>
+            <a href='#' className='nav-tab__link'>О проекте</a>
+          </li>
+          <li className='nav-tab-item'>
+            <a href='#' className='nav-tab__link'>Технологии</a>
+          </li>
+          <li className='nav-tab-item'>
+            <a href='#' className='nav-tab__link'>Студент</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 };
 
