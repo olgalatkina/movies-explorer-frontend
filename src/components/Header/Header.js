b05705235449faaa4a8193ab400dabc17148e30b
Olga Latkina <olga.latkina@gmail.com>
2022-10-22 07:25:25 +0700

    feat: add Header, Navigation and Burger markup and styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,39 @@
+import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import cn from 'classnames';
 import './Header.css';
+import logo from '../../images/logo.svg';
+import Navigation from '../Navigation/Navigation';
+import Burger from '../Burger/Burger';
 
 const Header = () => {
-  return (
-    <header className='header'>
-      <div className='header__content'>
+  const { pathname } = useLocation();
+  const [loggedIn, setLoggedIn] = useState(true);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const headerClassNames = cn('header', {
+    'header_theme_dark': pathname === '/',
+  });
+
+  return (
+    <header className={headerClassNames}>
+      <div className='header__content'>
+        <Burger loggedIn={loggedIn} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+        <Link to='/' className={loggedIn ? '' : 'header__logo-link'}>
+          <img src={logo} alt='Логотип учебного проекта' className='logo' />
+        </Link>
+        {loggedIn
+          ? <Navigation isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+          :
+          <nav className='header__navigation'>
+            <Link to='/signup' className='header__register-link'>
+              Регистрация
+            </Link>
+            <Link to='/signin' className='header__login-link'>
+              Войти
+            </Link>
+          </nav>
+        }
       </div>
     </header>
   )
